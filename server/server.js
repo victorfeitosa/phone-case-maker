dde8951f996c22d444300de2bc90beca988bbaa8
Victor Feitosa <victor.klngo@gmail.com>
2019-04-15 23:09:59 -0300

    set up node server

---
+++ server/server.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const formidable = require('formidable');
+
+const path = require('path');
+const hostname = process.env.HOST_NAME || 'localhost';
+const port = process.env.PORT || 8000;
+
+const app = express();
+const publicPath = path.join(__dirname, '..', 'dist');
+
+app.use(express.static(publicPath));
+
+app.post('/upload-background', (req, res) => {
+  new formidable.IncomingForm().parse(req)
+    .on('fileBegin', (name, file) => {
+      var fileExt = path.extname(file.name);
+
+      file.name = 'uploaded' + fileExt;
+      file.path = path.join(__dirname, 'uploads', file.name);
+    })
+    .on('file', (name, file) => {
+      res.write(JSON.stringify({ file: file.name }));
+    })
+    .on('end', () => {
+      res.end();
+    });
+});
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(publicPath, 'index.html'));
+});
+
+app.listen(port, () => {
+  console.log('Server is up on http://' + hostname + ':' + port);
+});
